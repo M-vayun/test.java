79bfae32f8f9cf18a5d5854983f73050417852c0
M-vayun <mullamvayun@gmail.com>
2023-08-03 10:03:52 -0500

    first

---
+++ test.java
@@ -0,0 +1,10 @@
+class test{
+    public static void main(String[] args){
+        int a=100;
+        Account a1=new Account(1);
+        Account a2=new Account(2);
+        System.Out.Println(a);
+        System.Out.Println(a1);
+        System.Out.Println(a2);
+    }
+}
